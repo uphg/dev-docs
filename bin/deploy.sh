649d3de29f1282b68fc0f64a0d24c92eb1206adb
lvheng <1834805770@qq.com>
2023-07-11 21:38:27 +0800

    添加部署脚本

---
+++ bin/deploy.sh
@@ -0,0 +1,29 @@
+# 确保脚本抛出遇到的错误
+set -e
+
+# 删除之前的文件
+rm -rf .vitepress/dist/
+
+echo '# build'
+
+# 生成静态文件
+pnpm docs:build
+
+# 进入生成的文件夹
+cd .vitepress/dist/
+
+# 如果是发布到自定义域名
+# echo 'uphgs.com' > CNAME &&
+
+echo '# git push'
+
+git init
+git add -A
+git commit -m 'update'
+
+git push -f git@github.com:uphg/dev-docs.git master:gh-pages
+# git push -f git@gitee.com:uphg/dev-docs.git master:gh-pages
+
+rm -rf .git
+
+cd -
